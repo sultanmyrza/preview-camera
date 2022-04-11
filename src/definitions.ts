7e5bcac79977b6b1cb3f7d595ac9398123f2cbba
sultanmyrza <sultanmyrza@gmail.com>
2022-04-11 21:31:57 +0800

    define plugin basic features

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,29 @@
+import { PluginListenerHandle } from '@capacitor/core';
+
+export interface CaptureResult {
+  /** File path for photo or video taken by camera. Example: file://your-app-dir/.../my-video.mp4 */
+  filePath?: string;
+  /** Error message if any */
+  errorMessage?: string;
+}
+
 export interface PreviewCameraPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  startPreview(): Promise<void>;
+  stopPreview(): Promise<void>;
+  takePhoto(): Promise<void>;
+  capturePhoto(): Promise<void>;
+  startRecord(): Promise<void>;
+  stopRecord(): Promise<void>;
+  flipCamera(): Promise<void>;
+  getFlashModes(): Promise<any>;
+  setFlashMode(): Promise<void>;
+  addListener(
+    eventName: 'captureVideoFinished',
+    listenerFunc: (data: CaptureResult) => void,
+  ): Promise<PluginListenerHandle> & PluginListenerHandle;
+  addListener(
+    eventName: 'capturePhotoFinished',
+    listenerFunc: (data: CaptureResult) => void,
+  ): Promise<PluginListenerHandle> & PluginListenerHandle;
 }
